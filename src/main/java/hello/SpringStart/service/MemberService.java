5de6d67e30127b4d0a00271b6a11f3d353567bf3
rong5026 <rong5026@naver.com>
2023-08-14 20:19:25 +0900

    Service Test

--- src/main/java/hello/SpringStart/service/MemberService.java
+++ src/main/java/hello/SpringStart/service/MemberService.java
@@ -4,14 +4,41 @@ import hello.SpringStart.domain.Member;
 import hello.SpringStart.repository.MemberRepository;
 import hello.SpringStart.repository.MemoryMemberRepository;
 
-public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+import java.util.List;
+import java.util.Optional;
 
-    // 회원가입
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     */
     public Long join(Member member)
     {
-        // 같은 이름이 있는 중복 회원
+        validateDuplicateMember(member); // 중복회원 검증
         memberRepository.save(member);
         return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                     throw new IllegalStateException("이미 존재하는 회원입니다");
+                 });
+    }
+
+    /**
+     * 전체회원조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
     }
 }
