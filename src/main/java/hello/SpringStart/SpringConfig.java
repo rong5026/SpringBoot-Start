a2f270b984ad3ff3eabac65525429ffbf5766487
rong5026 <rong5026@naver.com>
2023-08-17 17:33:24 +0900

    H2 DB연결

--- src/main/java/hello/SpringStart/SpringConfig.java
+++ src/main/java/hello/SpringStart/SpringConfig.java
@@ -1,14 +1,24 @@
 package hello.SpringStart;
 
+import hello.SpringStart.repository.JdbcMemberRepository;
 import hello.SpringStart.repository.MemberRepository;
 import hello.SpringStart.repository.MemoryMemberRepository;
 import hello.SpringStart.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // bean에 따로 등록하는 방법
 @Configuration
 public class SpringConfig {
+
+    DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -17,6 +27,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
+
 }
